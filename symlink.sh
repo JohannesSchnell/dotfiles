e3e604c29b4ee7c3e32faa83f7c73f0c2bcd907c
JohannesSchnell <j.schnell@52north.org>
2024-12-02 17:26:25 +0100

    neues

--- symlink.sh
+++ symlink.sh
@@ -2,8 +2,8 @@
 
 #symlinking configs, overwriting possible files
 #shellRC
-ln -sf ./.shell/.zshrc $HOME/.zshrc
-ln -sf ./.shell/.bashrc $HOME/dotfiles/.bashrc
+ln -sf $(pwd)/.shell/.zshrc $HOME/.zshrc
+ln -sf $(pwd)/.shell/.bashrc $HOME/dotfiles/.bashrc
 
 #
-ln -sf ./.shell $HOME/.shell
+ln -sf $(pwd)/.shell $HOME/.shell
