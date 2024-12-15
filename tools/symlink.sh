c37fa86b7b4dbd5bb7c87bdf2fe89b3b1bd9f94c
JohannesSchnell <j.schnell@52north.org>
2024-12-15 15:52:18 +0100

    added symlink all csgs

--- tools/symlink.sh
+++ tools/symlink.sh
@@ -9,17 +9,10 @@ ln -sf $(pwd)/.shell/.bashrc $HOME/.bashrc
 rm -rf $HOME/.shell
 ln -snf $(pwd)/.shell $HOME/.shell
 
-#config
-rm -rf $HOME/.config/kitty
-ln -snf $(pwd)/.config/kitty $HOME/.config/
-rm -rf $HOME/.config/ranger
-ln -snf $(pwd)/.config/ranger $HOME/.config/
-rm -rf $HOME/.config/i3
-ln -snf $(pwd)/.config/i3 $HOME/.config/
-rm -rf $HOME/.config/picom
-ln -snf $(pwd)/.config/picom $HOME/.config/
-rm -rf $HOME/.config/polybar
-ln -snf $(pwd)/.config/polybar $HOME/.config/
-rm -rf $HOME/.config/rofi
-ln -snf $(pwd)/.config/rofi $HOME/.config/
+# symlink all configs 
+CONFIGS=$(ls -d .config/*/)
+for cfg in $CONFIGS; do 
+    rm -rf "$HOME/$cfg"
+    ln -snf "$(pwd)/$cfg" "$HOME/.config/"
+done
 
